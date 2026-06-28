c16933168d4e8e3bb92db65fa1e8e68768906d1e
plaidroni <plaidroni@gmail.com>
2026-06-28 12:18:18 -0700

    fix pics

--- src/utils/Projects.tsx
+++ src/utils/Projects.tsx
@@ -23,7 +23,7 @@ export const projects = [
       url: "https://www.nasa.gov",
       roles: [{ name: "Internship", color: { color: "software-engineer", opacity: "strong" } }],
       period: "Aug 2026 - Present",
-      img: "public\\NASA_Wormball_logo.png",
+      img: "https://raw.githubusercontent.com/plaidroni/sevanevans.com/refs/heads/master/public/NASA_Wormball_logo.png",
       description: ``,
       techStack: [],
    },
@@ -32,7 +32,7 @@ export const projects = [
       url: "",
       roles: [{ name: "Student Researcher", color: { color: "co-founder", opacity: "strong" } }],
       period: "Nar 2026 - Present",
-      img: "../../public/NASA_Wormball_logo.png",
+      img: "https://raw.githubusercontent.com/plaidroni/sevanevans.com/refs/heads/master/public/NASA_Wormball_logo.png",
       description: `
 `,
       techStack: [],
@@ -42,7 +42,7 @@ export const projects = [
       url: "",
       roles: [{ name: "Stem Teacher", color: { color: "co-founder", opacity: "strong" } }],
       period: "Sept 2025 - May 2026",
-      img: "public/windtree_education_logo.jpg",
+      img: "https://raw.githubusercontent.com/plaidroni/sevanevans.com/refs/heads/master/public/windtree_education_logo.jpg",
       description: `I recently had the pleasure of participating in NASA's Community College Aerospace Scholars Robotics Competition at College of the Desert.
 
 The competition was an exceptional lesson in teamwork, adaptability, and systems thinking. As the competition neared its end, the team faced mounting pressure to design a claw with integrated sensors. Our initial design attempted to embed sensors into the claw, but the process took longer than anticipated, and we ran out of time before it could reliably grip objects. Rather than force a failing design, we pivoted to a plow, which required a rapid software update. With a minute left, we adapted the gyroscopic code to flip the targeted heading when an object was sensed and, despite having no time to test, placed the rover on the field for the final run—and thankfully, it worked amazingly. For my technical contributions, soft skills, and collaborative leadership, I was selected by our mentor as team MVP.
@@ -88,7 +88,7 @@ Communicate complex scientific principles clearly to students of varied experien
       url: "",
       roles: [{ name: "Physics A.S.", color: { color: "physics-as", opacity: "strong" } }],
       period: "August 2024 - Expected Spring 2027",
-      img: "public\\COD.jpg",
+      img: "https://raw.githubusercontent.com/plaidroni/sevanevans.com/refs/heads/master/public/COD.jpg",
       description: `During my Fall 2025 semester at College of the Desert, I founded and currently serve as President of the Physics Club. We have since grown to nearly 30 active members, have hosted multiple guest speakers from different fields, and organized several astronomy and physics-based field trips. In Spring 2026, we plan to fundraise, design, and engineer a small-scale rocket launch. I aim to inspire our members to pursue STEM and support their professional development by guiding them in creating personal online portfolios and connecting them with resources and school funding opportunities.`,
       techStack: [],
    },
@@ -108,7 +108,7 @@ Communicate complex scientific principles clearly to students of varied experien
          { name: "XCode", icon: <XCode /> },
          { name: "AndroidStudio", icon: <AndroidStudio /> },
       ],
-      img: "public/tigase.png",
+      img: "https://raw.githubusercontent.com/plaidroni/sevanevans.com/refs/heads/master/public/tigase.png",
       description: `I am currently excelling in the creation of multi-platform applications using Kotlin and Swift for platforms including Android, iOS, MacOS, and Windows. My strengths lie in adapting swiftly to emerging technologies and grasping new concepts at a rapid pace. I have been working in close collaboration with graphic designers, which has empowered us to produce eye-catching illustrations. Furthermore, my work extends to the design of visual layout mockups, utilizing tools such as Adobe Photoshop and Illustrator. Notably, I am deeply involved in creating and overhauling application user experiences to align with modern standards, thereby successfully attracting new audiences.`,
    },
    {
@@ -124,7 +124,7 @@ Communicate complex scientific principles clearly to students of varied experien
          { name: "CSS", icon: <CSS /> },
          { name: "React", icon: <ReactIcon /> },
       ],
-      img: "public/quick_draw.jpg",
+      img: "https://raw.githubusercontent.com/plaidroni/sevanevans.com/refs/heads/master/public/quick_draw.jpg",
       description: ``,
    },
    {
@@ -144,7 +144,7 @@ Communicate complex scientific principles clearly to students of varied experien
          { name: "React", icon: <ReactIcon /> },
          { name: "Express", icon: <Express /> },
       ],
-      img: "https://firebasestorage.googleapis.com/v0/b/bodymx-80bc1.appspot.com/o/sevanportfolio%2Fimageedit_6_6528806410.png?alt=media&token=d171f856-e3e4-4af4-aa12-27c83b674d86",
+      img: "",
       description: `As the Co-founder and Software Engineer of BodyMX, I've been creating a comprehensive digital platform that seamlessly connects fitness enthusiasts with trainers and wellness resources. Through innovation and determination, I've guided our team in designing an intuitive, responsive, and effective suite of fitness tools that benefit both users and professionals in the industry.
       
       In the creation of our website and mobile application, I leveraged a robust tech stack including React and TypeScript, alongside other cutting-edge technologies. React's reusable components and virtual DOM helped us craft a highly interactive user interface that not only runs smoothly but also makes for a delightful user experience. TypeScript, with its static typing, empowered us with better maintainability and debugging capabilities, resulting in safer and more reliable software.`,
@@ -178,7 +178,7 @@ Communicate complex scientific principles clearly to students of varied experien
          { name: "XCode", icon: <XCode /> },
          { name: "AndroidStudio", icon: <AndroidStudio /> },
       ],
-      img: "public/palm-desert-high.png",
+      img: "https://raw.githubusercontent.com/plaidroni/sevanevans.com/refs/heads/master/public/palm-desert-high.png",
       description: `During the PDHS Official School Application project, I developed crucial skills in cooperative problem-solving, communication, and rapid software development. As a collaborator, I contributed to a platform impacting students, teachers, and staff daily. Our challenge was maintaining consistent communication across the app's facets, which we overcame with modern development tools and a dedicated approach. This experience emphasized the importance of collaboration, precision, and user-centric design in real-world software development.`,
    },
 
@@ -188,7 +188,7 @@ Communicate complex scientific principles clearly to students of varied experien
       roles: [{ name: "1st Place", color: { color: "1st-place", opacity: "strong" } }],
       period: "Apr 2019",
       techStack: [{ name: "Java", icon: <Java /> }],
-      img: "https://firebasestorage.googleapis.com/v0/b/bodymx-80bc1.appspot.com/o/sevanportfolio%2Fimageedit_8_7526074017.png?alt=media&token=d5d62334-ff4c-47a6-b1c8-1904c61fc959",
+      img: "https://raw.githubusercontent.com/plaidroni/sevanevans.com/refs/heads/master/public/rcpc.png",
       description: `I had the thrilling experience of being part of the winning team at the 2019 RSPC Hackathon, a fast-paced 2-hour competition that pushed our abilities to their limits. We chose Java, a language we were confident and skilled in to do the competition in.
 
        The challenge was not just in coding, but in developing an innovative solution within a very limited time frame. Despite the pressures of time, we successfully solved all problems set in front of us with flying colors. Our win was not just a testament to our technical abilities in Java, but also our team coordination, quick decision-making, and capacity to innovate under pressure.`,
